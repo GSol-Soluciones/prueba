5c646abadbcc24970d94ade11e8f0e571effbeb7
GSol-Soluciones <76481334+GSol-Soluciones@users.noreply.github.com>
2021-12-14 08:41:27 +0100

    Added .py files

---
+++ home_background_enterprise/__manifest__.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+#????????????????????????????????????????????????????????????????????????????
+#?                                                                          ?
+#?           ,o888888o.      d888888o.       ,o888888o.     8 8888          ?
+#?          8888     `88.  .`8888:' `88.  . 8888     `88.   8 8888          ?
+#?       ,8 8888       `8. 8.`8888.   Y8 ,8 8888       `8b  8 8888          ?
+#?       88 8888           `8.`8888.     88 8888        `8b 8 8888          ?
+#?       88 8888            `8.`8888.    88 8888         88 8 8888          ?
+#?       88 8888             `8.`8888.   88 8888         88 8 8888          ?
+#?       88 8888   8888888    `8.`8888.  88 8888        ,8P 8 8888          ?
+#?       `8 8888       .8'8b   `8.`8888. `8 8888       ,8P  8 8888          ?
+#?          8888     ,88' `8b.  ;8.`8888  ` 8888     ,88'   8 8888          ?
+#?           `8888888P'    `Y8888P ,88P'     `8888888P'     8 888888888888  ? 
+#?                                                                          ?
+#?     SOFTWARE DEVELOPED AND SUPPORTED BY GSol Soluciones Informáticas     ?
+#?                       COPYRIGHT (C) 2020 - TODAY                         ?
+#?                           https://www.gsol.es                            ?
+#?                                                                          ?
+#????????????????????????????????????????????????????????????????????????????
+{
+    'name': 'Home Background Enterprise',
+    'version' : '14.1.1',
+    'summary': 'Change the Home Background color in Odoo Enterprise',
+    'description': """
+    """,
+    'author': "GSol Soluciones Informaticas",
+    'website': "https://www.gsol.es",
+    'category': 'Sale',
+    'depends' : ['web'],
+    'data': [
+        'views/assets.xml'
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
